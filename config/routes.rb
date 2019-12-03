fc867870e76034046beedfca2d3cc6a1a3709a42
Christos Kaltaveridis <aprositosakis@yahoo.gr>
2019-12-03 12:15:46 +0100

    FIX extra routes removed, only nessesary created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'islands/index'
-  get 'islands/show'
-  get 'islands/new'
-  get 'islands/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :islands
+  resources :islands, only: [:index, :show, :new, :create]
 end
