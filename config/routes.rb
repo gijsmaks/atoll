0cc8290da06ca6ebe400ee2e801802e6526e7f3b
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-05 14:03:28 +0100

    FIX: add show page to bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :islands, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :show]
   end
 end
