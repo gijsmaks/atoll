c604805bf9c72f0a29b57c8ee012816d7adcac54
Ben Toosey <benjamin.toosey@gmail.com>
2019-12-06 15:57:17 +0100

    changed seeds from 2s to 3s

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,40 +31,40 @@ Island.create!(user: gijs, name: 'Cave Cay', description: 'Rarely does an island
 
 Located in the heart of the Exumas and near to famed Musha Cay, Cave Cay is one of the most beautiful spots in the world and is totally self sufficient with water and power supplied by diesel generators, while solar energy can easily be made available.', price: '8000', location: "Caribbean", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Strum Island', description: 'Ideal for family getaways and corporate retreats, Strum island is an exclusive private island rental located in picturesque Mahone Bay, Nova Scotia.', price: '1600', location: "Canada", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Corfu', description: 'A beautiful 42 acres tropical island with villa & staff quarters set a midst coconut palms, shrubs and other native plants and trees. The island has a white soft sandy beach all around.
 
 A large 200 acre shallow turquoise lagoon with crystalline waters encircles the island. And the lagoon in its turn is fully encircled by a healthy house reef, which protects the lagoon and the island from eventual heavy sea.', price: '1500', location: "Greece", remote_photo_url: "https://source.unsplash.com/random?island")
 
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Schulzewerder Island', description: 'Professionally managed and run as a campground, Polands beautiful Schulzewerder Island is now for sale.
 
-The well-kept campground on 8.5-hectare Schulzewerder Island is situated directly on Lubie Lake. The only way to reach the campground is by ferry or by boat. The lake is nestled in a magnificent unspoiled landscape of hills and forests. The Lubiesee (17km long and 2.5 km wide) is located about 260km east of Berlin, within easy reach of a well-developed highway.', price: '1500', location: "Zlocieniec", remote_photo_url: "https://source.unsplash.com/random?island")
+The well-kept campground on 8.5-hectare Schulzewerder Island is situated directly on Lubie Lake. The only way to reach the campground is by ferry or by boat. The lake is nestled in a magnificent unspoiled landscape of hills and forests. The Lubiesee (17km long and 3.5 km wide) is located about 260km east of Berlin, within easy reach of a well-developed highway.', price: '1500', location: "Zlocieniec", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Vouvalos Island', description: 'This stunning private island is located in the Amvrakikos Gulf, a beautiful area located in northwestern Greece. Leading out into the Ionian Sea, the gulf is an idyllic, less-travelled area in a picturesque mountain setting.
 
 The island is more than 31 acres of olive groves, forests, hills and rocky cliffs. A highly unique island, Vouvalos has a striking and unusual shape providing natural harbours. There is also a lagoon in the centre of the island. Dolphins may often be found in the clean surrounding waters, and Vouvalos has beautiful sandy beaches to enjoy Greeces warm and sunny weather. Conveniently located, the island is close to an international airport and several small towns', price: '1500', location: "Greece", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Mola Selatan', description: 'Mola Selatan is surrounded by pink sands and turquoise subtropical waters, Windermere Island is just 20 miles off Sulawesi.', price: '500', location: "Indonesia", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Taveuni Island', description: 'A rare opportunity to acquire this renowned and fully established, award winning resort and spa. This premiere freehold island property is a unique offering beyond compare!
 
 Along with a list of international awards, Taveuni Island Resort & Spa’s most recent recognition, and one of its most prestigious, to start the year was the Trip Advisor ‘2017 Travelers Choice’ Award, reaffirming this premier resort and spa has all the elements synonymous with top luxury serene island retreats.', price: '1500', location: "Fiji", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Vadinolhu Island', description: 'This is a 150 room resort development with joint venture opportunity!
 
@@ -72,15 +72,15 @@ Awarded: 2009 (on conditional basis with CSR component)
 
 Lease period remaining: 18 years - there will be option to extend the head lease. There are chances to negotiate with authorities for further extension.', price: '1500', location: "Laamu Atoll", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'La Tortue', description: "Every fale is located, among the trees, on a white sandy beach, providing a truly peaceful, private, and relaxing beach experience. Uoleva was once listed by National Geographic as one of the top 10 island beach destinations in the world. The island is totally surround by sand and people love to take a day and walk the 6 mile (10 K) around the island to experience the total deserted island feel. La Tortue is far from the distractions and pace of the 21st century and a perfect place to reconnect with nature, your loved ones, and yourself.", price: '1500', location: "Caribbean", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Ko Kut', description: 'Incredible beautiful Island.', price: '1500', location: "Thailand", remote_photo_url: "https://source.unsplash.com/random?island")
 
-sleep(2)
+sleep(3)
 
 Island.create!(user: gijs, name: 'Leava', description: "This area is renowned for its beautiful coral reefs, as being a diver's paradise, and for the fascinating culture of some of its islands. Its has more than 600 islands, of which only 160 are inhabited. The coral reef system of Leava are some of the most bio-diverse in the world and as such attract equal attention from dive operators as well.", price: '1100', location: "Fiji", remote_photo_url: "https://source.unsplash.com/random?island")
 
