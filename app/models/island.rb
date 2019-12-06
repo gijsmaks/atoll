61456265913b1e1e3faddd0af6d52b90bf8aa31c
Ben Toosey <benjamin.toosey@gmail.com>
2019-12-06 14:16:46 +0100

    Functional seeds to work with map

--- app/models/island.rb
+++ app/models/island.rb
@@ -3,12 +3,12 @@ class Island < ApplicationRecord
   has_many :bookings
   mount_uploader :photo, PhotoUploader
 
-  geocoded_by :location
+  geocoded_by :name
   after_validation :geocode, if: :will_save_change_to_location?
 
   validates :name, presence: true, allow_blank: false
   validates :description, presence: true, allow_blank: false
   validates :price, presence: true, numericality: { only_integer: true }, allow_blank: false
   validates :photo, presence: true
-  validates :location, presence: true, uniqueness: true, allow_blank: false
+  validates :location, presence: true, allow_blank: false
 end
